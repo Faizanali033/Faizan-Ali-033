823a8e92b758436cb4c3a94c75fe0c5cc1165c93
Faizanali033 <sp24-bba-033@cuilahore.edu.pk>
2025-02-27 13:14:13 +0500

    033

---
+++ faizan.py
@@ -0,0 +1,8 @@
+a=input("enter first value:")
+b=input("enter the second value:")
+if int(a)>int(b):
+    print(a,"is greater")
+elif int(b)>int(a):
+    print(b,"is greater")
+else:
+    print("both are equal values")
